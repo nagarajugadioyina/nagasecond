ce8a7cea634dc6f87b9e85b542e8afc241e398cb
nagarjugadiboyina <nagaraju8555855@gmail.com>
2023-03-03 09:59:06 +0530

    second commit

---
+++ src/main/java/com/naga/App.java
@@ -0,0 +1,20 @@
+package com.naga;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("spconfig.xml");
+        
+        AdditionBean ab=(AdditionBean)ctx.getBean("id1");
+        
+        ab.printAddition();
+    }
+}
